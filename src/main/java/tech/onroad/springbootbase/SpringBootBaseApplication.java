57f651b27264bd1129735a44fdff2c19c1756a8e
onroadtech <onroad_tech@163.com>
2017-11-23 21:14:20 +0800

    Add a controller for demo.

--- src/main/java/tech/onroad/springbootbase/SpringBootBaseApplication.java
+++ src/main/java/tech/onroad/springbootbase/SpringBootBaseApplication.java
@@ -2,11 +2,19 @@ package tech.onroad.springbootbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootBaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBaseApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello world!";
 	}
 }
